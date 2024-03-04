c4f627bc6fe04f6948420a387529716188e1162a
Rohit Rahman <rohitrhmn208@gmail.com>
2024-03-04 17:06:00 +0530

    Updated package.json

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,33 +3,34 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 
-import packageJson from "./package.json" assert { type: "json" };
+import testLib from "./package.json" assert { type: "json" };
 
 export default [
-	{
-		input: "src/index.ts",
-		output: [
-			//   {
-			//     file: packageJson.main,
-			//     format: "cjs",
-			//     sourcemap: true,
-			//   },
-			{
-				file: packageJson.module,
-				format: "esm",
-				sourcemap: true
-			}
-		],
-		plugins: [
-			resolve(),
-			commonjs(),
-		 
-			typescript({ tsconfig: "./tsconfig.json" })
-		]
-	},
-	{
-		input: "dist/esm/types/index.d.ts",
-		output: [{ file: "dist/index.d.ts", format: "esm" }],
-		plugins: [dts()]
-	}
+  {
+    input: "src/index.ts",
+    output: [
+      //   {
+      //     file: packageJson.main,
+      //     format: "cjs",
+      //     sourcemap: true,
+      //   },
+      { file: testLib.main, format: "cjs", sourcemap: true },
+      {
+        file: testLib.module,
+        format: "esm",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      resolve(),
+      commonjs(),
+
+      typescript({ tsconfig: "./tsconfig.json" }),
+    ],
+  },
+  {
+    input: "dist/esm/types/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    plugins: [dts()],
+  },
 ];
